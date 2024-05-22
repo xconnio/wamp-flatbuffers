4ba76eb5e60ea91e00edbaec51a6a917aa26c90e
Omer Akram <omer@thing.com>
2024-05-23 02:20:31 +0500

    convert message parsers to individual functions

--- python/wampprotofbs/parsers/welcome.py
+++ python/wampprotofbs/parsers/welcome.py
@@ -1,14 +1,11 @@
 from flatbuffers import Builder, Table
 
 from wampproto.messages import Welcome, Message
-from wampprotofbs.parsers.interface import IMessageFBS
 
 
-class WelcomeParser(IMessageFBS):
-    @staticmethod
-    def from_fbs(msg: Table) -> Message:
-        raise NotImplementedError
+def from_fbs(msg: Table) -> Message:
+    raise NotImplementedError
 
-    @staticmethod
-    def to_fbs(welcome: Welcome, builder: Builder):
-        raise NotImplementedError
+
+def to_fbs(welcome: Welcome, builder: Builder):
+    raise NotImplementedError
